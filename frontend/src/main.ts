158db85329647510977ad71b89cdffe9bda19b69
Cecilia Edstein <cecilia.edstein@gmail.com>
2026-03-18 00:04:13 +0100

    feat: implement createHtml function for rendering todo items and handling updates/removals

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,22 @@
 import "./style.css";
+import { createTodo, getTodos } from "./services/todoService";
+import { createHtml } from "./utils/htmlUtils";
 
 document.getElementById("todoform")?.addEventListener("submit", async (e) => {
   e.preventDefault();
+
+  const theUserText = (document.getElementById("todotext") as HTMLInputElement)
+    .value;
+
+  const data = await createTodo(theUserText);
+  console.log(data);
+
+  (document.getElementById("todotext") as HTMLInputElement).value = "";
+
+  const todos = await getTodos();
+  createHtml(todos);
 });
+const todos = await getTodos();
+
+// Rita ut dem på skärmen
+createHtml(todos);
