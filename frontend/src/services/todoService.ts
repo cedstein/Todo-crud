8975ff05fe1d22c9e7cadc8bd2c43ada72759c64
Cecilia Edstein <cecilia.edstein@gmail.com>
2026-03-17 21:11:03 +0100

    feat: implement removeTodo function for deleting todo items

--- frontend/src/services/todoService.ts
+++ frontend/src/services/todoService.ts
@@ -1,5 +1,17 @@
 import type { Todo } from "../models/Todo";
 
+export const removeTodo = async (id: number) => {
+  try {
+    const response = await fetch("http://localhost:3000/todos/" + id, {
+      method: "DELETE",
+    });
+
+    return response.ok;
+  } catch {
+    return false;
+  }
+};
+
 export const updateTodo = async (id: number, todo: Todo) => {
   try {
     const response = await fetch(`http://localhost:3000/todos/${id}`, {
