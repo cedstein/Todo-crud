af208306027a00948b94c1dc4d75ba2978f980a5
Cecilia Edstein <cecilia.edstein@gmail.com>
2026-03-17 22:06:45 +0100

    feat: implement getTodos and getTodoById functions for fetching todos

--- frontend/src/services/todoService.ts
+++ frontend/src/services/todoService.ts
@@ -1,5 +1,22 @@
 import type { Todo } from "../models/Todo";
 
+export const getTodos = async () => {
+  try {
+    const response = await fetch("http://localhost3000/todos");
+    const data: Todo[] = await response.json();
+    return data;
+  } catch {
+    console.error("could not fetch data from API");
+    return [];
+  }
+};
+
+export const getTodoById = async (id: number) => {
+  const response = await fetch("http://localhost3000/todos" + id);
+  const data: Todo = await response.json();
+  return data;
+};
+
 export const createTodo = async (text: string) => {
   const response = await fetch("http://localhost3000/todos", {
     method: "POST",
